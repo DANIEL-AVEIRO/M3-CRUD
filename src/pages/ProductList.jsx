edfe1dbef304dbc8b79838b03becf10232aa13ee
Daniel <codedbydaniel@gmail.com>
2025-10-21 15:40:30 +0630

    latest

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -5,6 +5,7 @@ import { toast } from 'react-toastify'
 const ProductList = () => {
 
     const [products, setProducts] = useState([])
+    const [search, setSearch] = useState("")
 
     const saveData = JSON.parse(localStorage.getItem("products")) || []
     useEffect(() => {
@@ -18,15 +19,21 @@ const ProductList = () => {
         toast.success("Product is deleted successfully")
     }
 
+    const filteredProducts = products.filter((product) =>
+        product.name.toLowerCase().includes(search.toLowerCase()) || product.price.toString().includes(search.toString())
+    )
+
     return (
         <div>
             <h1 className='text-center text-3xl'>Product List</h1>
             <div className='flex justify-between'>
                 <Link to='/product/create/' className='px-6 py-4 inline-block text-white rounded-lg cursor-pointer bg-green-600'>Add New Product</Link>
                 <input
-                    type="text"
+                    type="search"
                     className="border border-solid border-black rounded-md py-4 px-6"
                     placeholder="Search Here"
+                    value={search}
+                    onChange={(e) => setSearch(e.target.value)}
                 />
             </div>
             <table className='w-full mt-10 p-4 border border-solid border-black'>
@@ -40,19 +47,25 @@ const ProductList = () => {
                 </thead>
                 <tbody className='p-4 border border-solid border-black'>
                     {
-                        products.map((product, index) => {
-                            return (
-                                <tr key={index} className='p-4 border border-solid border-black'>
-                                    <td className='p-4 border border-solid border-black text-center'>{index + 1}</td>
-                                    <td className='p-4 border border-solid border-black text-center'>{product.name}</td>
-                                    <td className='p-4 border border-solid border-black text-center'>{product.price}</td>
-                                    <td className='p-4 border border-solid border-black text-center'>
-                                        <Link to={`/product/update/${product.id}`} className='px-6 py-4 inline-block text-white rounded-lg cursor-pointer bg-yellow-500'>Edit</Link>
-                                        <button onClick={() => handleDelete(product.id)} className='px-6 py-4 inline-block text-white ms-4 rounded-lg cursor-pointer bg-red-500'>Delete</button>
-                                    </td>
-                                </tr>
-                            )
-                        })
+                        filteredProducts.length > 0 ? (
+                            filteredProducts.map((product, index) => {
+                                return (
+                                    <tr key={index} className='p-4 border border-solid border-black'>
+                                        <td className='p-4 border border-solid border-black text-center'>{index + 1}</td>
+                                        <td className='p-4 border border-solid border-black text-center'>{product.name}</td>
+                                        <td className='p-4 border border-solid border-black text-center'>{product.price}</td>
+                                        <td className='p-4 border border-solid border-black text-center'>
+                                            <Link to={`/product/update/${product.id}`} className='px-6 py-4 inline-block text-white rounded-lg cursor-pointer bg-yellow-500'>Edit</Link>
+                                            <button onClick={() => handleDelete(product.id)} className='px-6 py-4 inline-block text-white ms-4 rounded-lg cursor-pointer bg-red-500'>Delete</button>
+                                        </td>
+                                    </tr>
+                                )
+                            })
+                        ) : (
+                            <tr className='p-4 border border-solid border-black'>
+                                <td colSpan={4} className='p-4 border border-solid border-black text-center'>No products found</td>
+                            </tr>
+                        )
                     }
                 </tbody>
             </table>
